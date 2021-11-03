0618b7792d930e48ce48c6886e9278d96e8b4d23
Whale <whalebox4.5@gmail.com>
2021-11-03 13:21:11 +0800

    uuu

--- nmea/nmea_test.py
+++ nmea/nmea_test.py
@@ -6,9 +6,9 @@ while True:
     port="/dev/ttyAMA0"
     ser= Serial(port, baudrate=9600, timeout=0.5)
     dataout = pynmea2.NMEAStreamReader()
-    newdata = ser.readline().decode('utf-8')
+    newdata = ser.readline()
     print(newdata)
-    if newdata[0:6] == "$GPRMC":
+    if newdata[0:6] == b"$GPRMC":
         newmsg = pynmea2.parse(newdata)
         lat = newmsg.latitude
         lng = newmsg.longitude
