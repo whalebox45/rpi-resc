64c6a6c456a1f2ec21f7d1862f11c6edc455e850
Whale <whalebox4.5@gmail.com>
2021-11-03 13:22:53 +0800

    ascii

--- nmea/nmea_test.py
+++ nmea/nmea_test.py
@@ -9,7 +9,7 @@ while True:
     newdata = ser.readline()
     print(newdata)
     if newdata[0:6] == b"$GPRMC":
-        newmsg = pynmea2.parse(newdata)
+        newmsg = pynmea2.parse(newdata.decode('ascii'))
         lat = newmsg.latitude
         lng = newmsg.longitude
         gps = "Lat: " + f'{lat}' + "Lng: " + f'{lng}'
