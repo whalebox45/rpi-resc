a2605025d23afd658388ea4bb86c715d5de969c3
Whale <whalebox4.5@gmail.com>
2021-11-08 14:23:44 +0800

    license

--- p2p/target2.py
+++ p2p/target2.py
@@ -2,28 +2,6 @@
 
 """ A simple beacon transmitter class to send a 1-byte message (0x0f) in regular time intervals. """
 
-# Copyright 2015 Mayer Analytics Ltd.
-#
-# This file is part of pySX127x.
-#
-# pySX127x is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public
-# License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
-# version.
-#
-# pySX127x is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
-# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
-# details.
-#
-# You can be released from the requirements of the license by obtaining a commercial license. Such a license is
-# mandatory as soon as you develop commercial activities involving pySX127x without disclosing the source code of your
-# own applications, or shipping pySX127x with a closed source product.
-#
-# You should have received a copy of the GNU General Public License along with pySX127.  If not, see
-# <http://www.gnu.org/licenses/>.
-
-# usage:
-# python p2p_send.py -f 433 -b BW125 -s 12
-
 import sys 
 from time import sleep
 sys.path.insert(0, '..')        
