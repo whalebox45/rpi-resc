36674831403f2f103ea6849396152e2fcc2aecb3
Whale <whalebox4.5@gmail.com>
2021-11-08 14:06:18 +0800

    loop

--- p2p/seeker_target.py
+++ p2p/seeker_target.py
@@ -15,25 +15,20 @@ class LoRaBeacon(LoRa):
         self.set_dio_mapping([1,0,0,0,0,0])
     
     def on_tx_done(self):
-        global args
         self.set_mode(MODE.STDBY)
         self.clear_irq_flags(TxDone=1)
         sys.stdout.flush()
         self.tx_counter += 1
         sys.stdout.write("\rtx #%d" % self.tx_counter)
-        if args.single:
-            print
-            sys.exit(0)
         BOARD.led_off()
-        sleep(args.wait)
-        rawinput = input(">>> ")
+        sleep(1)
+        rawinput = "transmit ok"
         data = [int(hex(ord(c)), 0) for c in rawinput]
         self.write_payload(data)
         BOARD.led_on()
         self.set_mode(MODE.TX)
 
     def start(self):
-        global args
         sys.stdout.write("\rstart")
         self.tx_counter = 0
         BOARD.led_on()
@@ -55,10 +50,9 @@ def main():
         lora_t.set_pa_config(pa_select=1)
 
         assert(lora_t.get_agc_auto_on() == 1)
-        
+
         lora_t.start()
         lora_t.set_mode(MODE.SLEEP)
         BOARD.teardown()
-        pass
 
 main()
