ba1773cf99a70683f1a853e2c08a87c3aa21a517
Whale <whalebox4.5@gmail.com>
2021-11-08 12:42:07 +0800

    tt

--- p2p/seeker2.py
+++ p2p/seeker2.py
@@ -99,10 +99,12 @@ class LoRaRcvCont(LoRa):
             self.set_dio_mapping([0,0,0,0,0,0])
             self.set_mode(MODE.RXCONT)
             print("\nRX mode")
+
             t_start = time.time()
             t_end = time.time()
+
             while t_end - t_start < 3:
-                
+                print("test")
                 rssi_value = self.get_rssi_value()
                 status = self.get_modem_status()
                 sys.stdout.flush()
