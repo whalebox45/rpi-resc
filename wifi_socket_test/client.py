71ac975d814f67cee3eaaf6ac99c7881a926ef1b
Whale <whalebox4.5@gmail.com>
2022-04-13 23:19:02 +0800

    byte test

--- wifi_socket_test/client.py
+++ wifi_socket_test/client.py
@@ -31,7 +31,7 @@ def write():
             message_input = input('')
             message = '{} ({}): {}'.format(nickname,str(client.getsockname()), message_input)
             # client.send(message.encode('utf-8'))
-            client.send("test")
+            client.send("test".encode('utf-8'))
         except Exception as e:
             print(str(e))
             client.close()
