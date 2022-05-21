371f0a99b35b5ff2dd086eb115fa9158c1f190a5
Whale <whalebox4.5@gmail.com>
2022-05-21 17:20:39 +0800

    reset main

---
+++ LoraRescuer.py
@@ -0,0 +1,43 @@
+import time
+from SX127x.LoRa import *
+
+class LoraRescuer(LoRa):
+
+    rx_data = ''
+    tx_data = 'test tx'
+
+    def __init__(self):
+        super(LoraRescuer, self).__init__(verbose=False)
+
+        self.set_mode(MODE.SLEEP)
+        self.set_dio_mapping([0,0,0,0,0,0])
+        
+        self.reset_ptr_rx()
+        self.tx_data = 'test tx'
+    
+    
+    '''
+        當LoRa模式設為RXCONT時便會反覆執行這個方法
+    '''
+    def on_rx_done(self):
+        self.set_mode(MODE.RXCONT)
+
+        self.clear_irq_flags(RxDone=1)
+        payload = self.read_payload(nocheck=True)
+        self.rx_data = ''.join([chr(c) for c in payload])
+
+        print(self.rx_data)
+
+        self.set_mode(MODE.STDBY)
+    
+    
+    '''
+        當LoRa模式設為TX時便會反覆執行這個方法
+    '''
+    def on_tx_done(self):
+        self.clear_irq_flags(TxDone=1)
+
+        self.write_payload(self.tx_data)
+        self.set_mode(MODE.TX)
+        time.sleep(1)
+        
--- lora_wifi_switch/LoRaRescuer.py
+++ lora_wifi_switch/LoRaRescuer.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 
-from enum import Enum, unique
-from random import randrange
-import sys
 import time
+from random import randrange
 
+import sys
+     
 from SX127x.LoRa import *
 
 from SystemInfo import get_serial, get_mac_address, get_hostname
@@ -16,114 +16,86 @@ rescuer_data = dict({
     }
 )
 
-@unique
-class LoRaSignalMode(Enum):
-    blank = 0
-    rx = 1
-    tx = 2
-    stdby = 3
-
 
 class LoRaRescuer(LoRa):
 
-    tx_string = str("")
-    rx_string = str("")
-
     def __init__(self, verbose=False):
         super(LoRaRescuer, self).__init__(verbose)
-
-        self.mode_switch = LoRaSignalMode.blank
-        self.transmit_str = str("")
-        
         self.set_mode(MODE.SLEEP)
         self.set_dio_mapping([0,0,0,0,0,0])
-
-        
-        self.reset_ptr_rx()
         
 
     def on_rx_done(self):
-        self.set_mode(MODE.RXCONT)
-        # BOARD.led_on()
+        self.set_mode(MODE.STDBY)
+        BOARD.led_on()
         print("\nRxDone")
         self.clear_irq_flags(RxDone=1)
         payload = self.read_payload(nocheck=True)
         data = ''.join([chr(c) for c in payload])
         
         # print(payload)
-        self.rx_string = data
-        print(self.rx_string)
-
-        # self.reset_ptr_rx()
+        print(f'\n{data}')
+ 
+        self.reset_ptr_rx()
         BOARD.led_off()
-        self.set_mode(MODE.STDBY)
+        self.set_mode(MODE.RXCONT)
 
     def on_tx_done(self):
         
-        # self.set_mode(MODE.STDBY)
+        self.set_mode(MODE.STDBY)
         self.clear_irq_flags(TxDone=1)
         
+        self.tx_counter += 1
         
-        
-        # BOARD.led_off()
+        BOARD.led_off()
         
         
         transmit_str = f'{rescuer_data}'
 
-        print(f"\n{transmit_str}")
+        print(f"\ntx #{self.tx_counter}: {transmit_str}")
 
         data = [ord(c) for c in transmit_str]
 
         self.write_payload(data)
-        # BOARD.led_on()
+        BOARD.led_on()
         self.set_mode(MODE.TX)
-        time.sleep(1)
+        time.sleep(0.25)
 
-
-
-
-
-def lora_receive(lora:LoRaRescuer,verbose=False):
-
-    lora.reset_ptr_rx()
-    lora.set_mode(MODE.STDBY)
-    lora.set_dio_mapping([0,0,0,0,0,0])
-    lora.set_mode(MODE.RXCONT)
+def lora_start(lora:LoRa):
     
-    print("\nRX mode")
+    lora.tx_counter = 0
+    lora.reset_ptr_rx()
+    while True:
+        
+        lora.set_mode(MODE.STDBY)
 
-    # Keep in receive mode when it's not switched
-    while lora.mode_switch is LoRaSignalMode.rx:
+        lora.set_dio_mapping([0,0,0,0,0,0])
+        lora.set_mode(MODE.RXCONT)
+        
+        # Start RX mode
+        print("\nRX mode")
 
-        if verbose:
+        t_start = time.time()
+        t_end = time.time()
+        rx_time = randrange(5,11)
+
+        # RX mode in 5~11 seconds
+        while t_end - t_start <= rx_time:
             rssi_value = lora.get_rssi_value()
             status = lora.get_modem_status()
             sys.stdout.flush()
             sys.stdout.write("\r%d %d %d" % (rssi_value, status['rx_ongoing'], status['modem_clear']))
-        
-
-    # Sleep 1 second
-    lora.set_mode(MODE.SLEEP)
-    time.sleep(0.5)
+            
+            t_end = time.time()
 
 
-
-
-
-
-def lora_transmit(lora:LoRaRescuer):
-    lora.set_mode(MODE.STDBY)
-    lora.set_dio_mapping([1,0,0,0,0,0])
-    lora.set_mode(MODE.TX)
-    
-    print("\nTX mode")
-
-    # Keep in transmit mode when it's not switched
-    while lora.mode_switch is LoRaSignalMode.tx:
+        # Sleep 1 second and switch to TX mode
+        lora.set_mode(MODE.SLEEP)
         time.sleep(1)
-
-    lora.reset_ptr_rx()
-
-    # Sleep 1 second
-    lora.set_mode(MODE.SLEEP)
-    time.sleep(0.5)
+        print("\nTX mode")
+        lora.set_dio_mapping([1,0,0,0,0,0])
+        lora.set_mode(MODE.TX)
+        
+        # TX mode in 6 seconds
+        time.sleep(6)
+        lora.reset_ptr_rx()
