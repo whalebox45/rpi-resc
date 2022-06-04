579d06325df4d1911cb49a289d01d2ba75b446ef
Whale <whalebox4.5@gmail.com>
2022-06-05 00:11:27 +0800

    rx print test

--- SocketRescuer.py
+++ SocketRescuer.py
@@ -16,7 +16,7 @@ class SocketRescuer:
         while True:
             msg, addr = self.socket.recvfrom(1024)
             self.rx_data = msg.decode()
-            # print("Socket RX:%s" % self.rx_data)
+            print("Socket RX:%s" % self.rx_data)
             if addr not in self.sock_client_list:
                 self.sock_client_list.append(addr)
             
