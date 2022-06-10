ae43d355529161b8c9979115e3bdbeb1b0ff8751
Whale <whalebox4.5@gmail.com>
2022-06-10 14:25:15 +0800

    nmea test except handle

--- nmea/nmea_test.py
+++ nmea/nmea_test.py
@@ -2,16 +2,19 @@ from serial import Serial
 import pynmea2
 
 while True:
-    port = "/dev/ttyAMA0"
-    ser = Serial(port, baudrate=9600, timeout=0.5)
-    dataout = pynmea2.NMEAStreamReader()
-    newdata = ser.readline()
-    # print(newdata)
+    try:
+        port = "/dev/ttyAMA0"
+        ser = Serial(port, baudrate=9600, timeout=0.5)
+        dataout = pynmea2.NMEAStreamReader()
+        newdata = ser.readline()
+        # print(newdata)
 
-    
-    if newdata[0:6] == b"$GPRMC":
-        newmsg = pynmea2.parse(newdata.decode('ascii'))
-        lat = newmsg.latitude
-        lng = newmsg.longitude
-        gps = "Lat: " + f'{lat}' + "Lng: " + f'{lng}'
-        print(gps)
+        
+        if newdata[0:6] == b"$GPRMC":
+            newmsg = pynmea2.parse(newdata.decode('ascii'))
+            lat = newmsg.latitude
+            lng = newmsg.longitude
+            gps = "Lat: " + f'{lat}' + "Lng: " + f'{lng}'
+            print(gps)
+    except:
+        pass
